271b95fd572d2603a52c3bd55e7357d825ed7eb2
Restkand <restkand07@gmail.com>
2024-06-16 11:27:53 +0700

    Update Header Home

---
+++ src/pages/Design.js
@@ -0,0 +1,12 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Design = () => {
+return (
+<div className="text-center">
+    <h2 className="text-2xl font-bold">Design Page</h2>
+</div>
+);
+};
+
+export default Design;
